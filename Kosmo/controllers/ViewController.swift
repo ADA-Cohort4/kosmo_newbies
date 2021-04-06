9d4534ef5609f0e53e1878b160c62f472aa99387
renatafel <renata.flc23@gmail.com>
2021-04-06 17:06:55 +0700

    mau coba push sekali hehe

--- Kosmo/controllers/ViewController.swift
+++ Kosmo/controllers/ViewController.swift
@@ -9,10 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var successTitle: UILabel!
+    @IBOutlet weak var succesTwoTitle: UILabel!
+    @IBOutlet weak var explainTitle: UILabel!
+    @IBOutlet weak var kembaliButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         // test
+        
+    
     }
 
 
