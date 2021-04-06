d3e300ebe99a583e863bd7fc452a2ec9a4c64c47
azrullahk <azrullahkainage@gmail.com>
2021-04-06 08:25:56 +0700

    Initial Commit

---
+++ Kosmo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kosmo
+//
+//  Created by Azrullah Kainage on 06/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
