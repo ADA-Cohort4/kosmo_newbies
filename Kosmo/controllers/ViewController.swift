352c07bbbbb500cb771c31199cbcf339977e20c8
renatafel <renata.flc23@gmail.com>
2021-04-07 09:08:00 +0700

    ini yang bener hehe

--- Kosmo/controllers/ViewController.swift
+++ Kosmo/controllers/ViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var successTitle: UILabel!
-    @IBOutlet weak var succesTwoTitle: UILabel!
+    @IBOutlet weak var subTitle: UILabel!
     @IBOutlet weak var explainTitle: UILabel!
-    @IBOutlet weak var kembaliButton: UIButton!
+    @IBOutlet weak var backhomeButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
