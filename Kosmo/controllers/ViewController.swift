6ca1f1626e03b3c9d3db45534fc6dffb61264133
azrullahk <azrullahkainage@gmail.com>
2021-04-08 22:21:11 +0700

    Embed video dan buat onboarding page

--- Kosmo/controllers/ViewController.swift
+++ Kosmo/controllers/ViewController.swift
@@ -6,15 +6,31 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player = AVPlayer()
+    var playerViewController = AVPlayerViewController()
+    @IBOutlet weak var thumbnail: UIView!
+    @IBOutlet weak var buttonThumbnail: UIButton!
+    
+    @IBAction func playVideo (_ sender: UIView) {
+        let videoPath = Bundle.main.path(forResource: "example", ofType: "mp4")
+        let videoPathURL = URL(fileURLWithPath: videoPath!)
+        player = AVPlayer(url: videoPathURL)
+        playerViewController.player = player
+        
+        self.present(playerViewController, animated: true, completion: {
+            self.playerViewController.player?.play()
+        })
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        // test
     }
-
-
+    
+    
 }
 
