5377addb4f607ca1cbee646701031664ef3cea83
azrullahk <azrullahkainage@gmail.com>
2021-04-13 08:42:16 +0700

    tower of hanoi DONE (#6)
    * Lengkapin Drag and Drop
    
    * Menara Hanoi Done
    
    - add logic to tower of hanoi
    
    * Optimize TOH code
    
    - optimize code on TOH vc
    
    * add tower Label
    
    Co-authored-by: ivanvsigit <ivanvsigit@gmail.com>

--- Kosmo/controllers/TOHViewController.swift
+++ Kosmo/controllers/TOHViewController.swift
@@ -24,8 +24,19 @@ class TOHViewController: UIViewController {
     @IBOutlet weak var Pan1: UIPanGestureRecognizer!
     @IBOutlet weak var Pan2: UIPanGestureRecognizer!
     @IBOutlet weak var Pan3: UIPanGestureRecognizer!
+    @IBOutlet weak var btnStack1: UIButton!
+    @IBOutlet weak var btnStack2: UIButton!
+    @IBOutlet weak var btnStack3: UIButton!
+    
     
     private var initialCenter: CGPoint = .zero
+    
+    var disks = [[2,1,0],[],[]]
+    var lastPos: CGPoint = .zero
+    var prevStack: Int = 0
+    var nextStack: Int = 0
+    var diskNumber: Int = 0
+    var WhichDisk: UIView!
     
     private let pannableView: UIView = {
         // Initialize View
@@ -42,7 +53,6 @@ class TOHViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
 //        KosmoBG.image = #imageLiteral(resourceName: "KosmoBG")
         CheckBtn.layer.cornerRadius = 10
         GameView.layer.cornerRadius = 10
@@ -63,113 +73,114 @@ class TOHViewController: UIViewController {
 //        Disk1.center = view.center
         
         // Initialize Swipe Gesture Recognizer
-        let panGestureRecognizer1 = UIPanGestureRecognizer(target: self, action: #selector(didPan1(_:)))
-        let panGestureRecognizer2 = UIPanGestureRecognizer(target: self, action: #selector(didPan2(_:)))
-        let panGestureRecognizer3 = UIPanGestureRecognizer(target: self, action: #selector(didPan3(_:)))
+        let panGestureRecognizer1 = UIPanGestureRecognizer(target: self, action: #selector(didPan(_:)))
+        let panGestureRecognizer2 = UIPanGestureRecognizer(target: self, action: #selector(didPan(_:)))
+        let panGestureRecognizer3 = UIPanGestureRecognizer(target: self, action: #selector(didPan(_:)))
         
         // Add Swipe Gesture Recognizer
 //        pannableView.addGestureRecognizer(panGestureRecognizer)
-        Disk1.addGestureRecognizer(panGestureRecognizer1)
-        Disk2.addGestureRecognizer(panGestureRecognizer2)
-        Disk3.addGestureRecognizer(panGestureRecognizer3)
+        btnStack1.addGestureRecognizer(panGestureRecognizer1)
+        btnStack2.addGestureRecognizer(panGestureRecognizer2)
+        btnStack3.addGestureRecognizer(panGestureRecognizer3)
     }
     
-    @objc private func didPan1(_ sender: UIPanGestureRecognizer) {
+    @IBAction func btn1 (_ sender: UIButton) {
+        let arr = self.disks[0]
+        setDiskAndStack(arr, 0)
+    }
+    
+    @IBAction func btn2 (_ sender: UIButton) {
+        let arr = self.disks[1]
+        setDiskAndStack(arr, 1)
+    }
+    
+    @IBAction func btn3 (_ sender: UIButton) {
+        let arr = self.disks[2]
+        setDiskAndStack(arr, 2)
+    }
+    
+    @IBAction func checkAnswer (_ sender: UIButton) {
+        let endStack = self.disks[2]
+        let answer = [2,1,0]
+        if (endStack == answer) {
+            performSegue(withIdentifier: "goToSuccess", sender: nil)
+        }
+    }
+    
+    func setDiskAndStack (_ arr: [Int], _ stack: Int) {
+        if (arr.count > 0) {
+            self.prevStack = stack
+            if (arr[arr.count - 1] == 0) {
+                self.WhichDisk = self.Disk1
+                self.diskNumber = 0
+            } else if (arr[arr.count - 1] == 1) {
+                self.WhichDisk = self.Disk2
+                self.diskNumber = 1
+            } else if (arr[arr.count - 1] == 2) {
+                self.WhichDisk = self.Disk3
+                self.diskNumber = 2
+            }
+        }
+    }
+    
+    func moveDisk (_ stack: Int, _ stackUIPosition: CGFloat) {
+        let middleStack = self.disks[stack]
+        let middleStackLength = self.disks[stack].count
+        if ( middleStackLength == 0) {
+            self.disks[stack].append(self.diskNumber)
+            self.WhichDisk.center.x = stackUIPosition
+            self.WhichDisk.center.y = 235
+        } else {
+            if (middleStack[middleStackLength - 1] > self.diskNumber) {
+                if (middleStackLength == 1) {
+                    self.WhichDisk.center.y = 220
+                } else if (middleStackLength == 2) {
+                    self.WhichDisk.center.y = 205
+                }
+                self.WhichDisk.center.x = stackUIPosition
+                self.disks[stack].append(self.diskNumber)
+            } else {
+                self.WhichDisk.center = self.lastPos
+                self.disks[self.prevStack].append(self.diskNumber)
+            }
+        }
+        print(self.disks)
+    }
+    
+    
+    @objc private func didPan(_ sender: UIPanGestureRecognizer) {
         switch sender.state {
             case .began:
-                initialCenter = Disk1.center
+                initialCenter = WhichDisk.center
+                lastPos = WhichDisk.center
+                let new = self.disks[self.prevStack].popLast()
+                print(new ?? "Disk Error")
             case .changed:
                 let translation = sender.translation(in: view)
-
-                Disk1.center = CGPoint(x: initialCenter.x + translation.x,
+                WhichDisk.center = CGPoint(x: initialCenter.x + translation.x,
                                               y: initialCenter.y + translation.y)
             case .ended,
                  .cancelled:
-                if ((self.Disk1.frame.midX >= self.view.layer.frame.width / 3) && (Disk1.center.x < 220)){
+                if ((self.WhichDisk.frame.midX >= self.view.layer.frame.width / 3) && (WhichDisk.center.x < 220)){
                     UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseIn, animations: {
-                            self.Disk1.center.x = 171
+                        self.moveDisk(1, 171)
                     }, completion: nil)
-                } else if Disk1.center.x >= 220 {
+                } else if WhichDisk.center.x >= 220 {
                     UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseIn, animations: {
-                            self.Disk1.center.x = 279
+                        self.moveDisk(2, 279)
                     }, completion: nil)
-                } else if Disk1.center.x < 120{
+                } else if WhichDisk.center.x < 120{
                     UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseIn, animations: {
-                            self.Disk1.center.x = 67
+                        self.moveDisk(0, 67)
                     }, completion: nil)
                 } else {
                     UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: [.curveEaseInOut]) {
-                            self.Disk1.center = self.initialCenter
+                            self.WhichDisk.center = self.initialCenter
                     }
                 }
             default:
                 break
             }
     }
-    
-    @objc private func didPan2(_ sender: UIPanGestureRecognizer) {
-        switch sender.state {
-            case .began:
-                initialCenter = Disk2.center
-            case .changed:
-                let translation = sender.translation(in: view)
-
-                Disk2.center = CGPoint(x: initialCenter.x + translation.x,
-                                              y: initialCenter.y + translation.y)
-            case .ended,
-                 .cancelled:
-                if ((self.Disk2.frame.midX >= self.view.layer.frame.width / 3) && (Disk2.center.x < 220)){
-                    UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseIn, animations: {
-                            self.Disk1.center.x = 171
-                    }, completion: nil)
-                } else if Disk2.center.x >= 220 {
-                    UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseIn, animations: {
-                            self.Disk1.center.x = 279
-                    }, completion: nil)
-                } else if Disk2.center.x < 120{
-                    UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseIn, animations: {
-                            self.Disk2.center.x = 67
-                    }, completion: nil)
-                } else {
-                    UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: [.curveEaseInOut]) {
-                            self.Disk2.center = self.initialCenter
-                    }
-                }
-            default:
-                break
-            }
-    }
-    
-    @objc private func didPan3(_ sender: UIPanGestureRecognizer) {
-        switch sender.state {
-            case .began:
-                initialCenter = Disk3.center
-            case .changed:
-                let translation = sender.translation(in: view)
-
-                Disk3.center = CGPoint(x: initialCenter.x + translation.x,
-                                              y: initialCenter.y + translation.y)
-            case .ended,
-                 .cancelled:
-                UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: [.curveEaseInOut]) {
-                    self.Disk3.center = self.initialCenter
-                }
-            default:
-                break
-            }
-    }
-    
-    class ImgData : ObservableObject{
-        @Published var totalImages : [Img] = [
-            Img(id: 0, image: "Disk1"),Img(id: 0, image: "Disk2"),Img(id: 0, image: "Disk3")
-        ]
-    }
-    
-    struct  Img : Identifiable {
-        var id : Int
-        var image : String
-    }
-
-
 }
 
